945a5531900e94e9194725bd0fa634aa766d4740
AntiDivis <134837223+AntiDivis@users.noreply.github.com>
2025-01-23 14:08:59 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+
+        //The Inputs
+        System.out.println("Input First Number:");
+        double X = scan.nextDouble();
+        System.out.println("Input Second Number:");
+        double Y = scan.nextDouble();
+
+        //The Outputs
+        System.out.println("Addition: " + (X + Y));
+        System.out.println("Subtraction" + (X - Y));
+        System.out.println("Multiplication: " + (X * Y));
+        System.out.println("Division: " + (X / Y));
     }
+
 }
